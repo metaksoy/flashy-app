4410fca64b2fe6f1776a4d1a1a1d76c9ce18ed49
metaksoy <metaksoy@example.com>
2025-10-19 01:41:22 +0300

    Fix Apollo Server Express package name and import

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
 const { ApolloServer } = require("@apollo/server");
-const { expressMiddleware } = require("@apollo/server/express4");
+const { expressMiddleware } = require("@apollo/server-express");
 const { typeDefs } = require("./schema");
 const resolvers = require("./resolvers");
 const jwt = require("jsonwebtoken");
