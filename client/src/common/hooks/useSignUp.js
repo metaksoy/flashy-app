710955511072fa6dbad20d074d05d78d66091c16
metaksoy <metaksoy@example.com>
2025-10-19 17:36:42 +0300

    feat: Email kullanımda hatası için Türkçe mesaj eklendi

--- client/src/common/hooks/useSignUp.js
+++ client/src/common/hooks/useSignUp.js
@@ -22,6 +22,15 @@ export const useSignUp = () => {
         } else {
           client.resetStore().then(() => navigate("/", { replace: true }));
         }
+      })
+      .catch((error) => {
+        // Email zaten kullanılıyor hatası
+        if (error.graphQLErrors?.some(err => err.extensions?.code === "EMAIL_ALREADY_EXISTS")) {
+          toast.error("Bu Eposta kullanılmaktadır!");
+        } else {
+          // Diğer hatalar
+          toast.error("Bir hata oluştu. Lütfen tekrar deneyin.");
+        }
       });
   };
   return signup;
