1229b3252f10ad62f80bf2e381e861b79bd6a63a
metaksoy <metaksoy@example.com>
2025-10-19 17:46:35 +0300

    fix: Email kontrolü düzeltildi - 'This Email Current' mesajı eklendi
    - try/catch yapısına geçildi
    - Console'da GraphQL error görünmüyor
    - Email duplicate hatası: 'This Email Current'
    - Diğer hatalar: 'Something went wrong'

--- client/src/common/hooks/useSignUp.js
+++ client/src/common/hooks/useSignUp.js
@@ -7,31 +7,28 @@ export const useSignUp = () => {
   const navigate = useNavigate();
 
   const signup = async (variables) => {
-    client
-      .mutate({
+    try {
+      const { data } = await client.mutate({
         mutation: gql`
           mutation createUser($email: String!, $password: String!) {
             createUser(email: $email, password: $password)
           }
         `,
         variables,
-      })
-      .then((data) => {
-        if (data.data === null) {
-          toast.error("Something went wrong :(");
-        } else {
-          client.resetStore().then(() => navigate("/", { replace: true }));
-        }
-      })
-      .catch((error) => {
-        // Email zaten kullanılıyor hatası
-        if (error.graphQLErrors?.some(err => err.extensions?.code === "EMAIL_ALREADY_EXISTS")) {
-          toast.error("Bu Eposta kullanılmaktadır!");
-        } else {
-          // Diğer hatalar
-          toast.error("Bir hata oluştu. Lütfen tekrar deneyin.");
-        }
       });
+      
+      // Başarılı kayıt
+      await client.resetStore();
+      navigate("/", { replace: true });
+    } catch (error) {
+      // Email zaten kullanılıyor hatası
+      if (error.graphQLErrors?.some(err => err.extensions?.code === "EMAIL_ALREADY_EXISTS")) {
+        toast.error("This Email Current");
+      } else {
+        // Diğer hatalar
+        toast.error("Something went wrong. Please try again.");
+      }
+    }
   };
   return signup;
 };
